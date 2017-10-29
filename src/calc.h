eff3c3b9ad20e35a9da9744dfd87686c5e6f263a
Dai, Pengcheng <13151098@buaa.edu.cn>
2017-10-29 19:53:11 +0800

    correct some error.

--- src/calc.h
+++ src/calc.h
@@ -8,6 +8,7 @@
 inline vec3d force2_Van(vec3d dest, vec3d source, double sigma, double epsilon) {
 	auto r = dest - source;
 	auto r_mod = r.abs();
+	auto r_2 = r_mod * r_mod;
 	auto r_6 = pow(r_mod, 6);
 	auto r_8 = r_6 * r_2;
 	auto sigma_6 = pow(sigma, 6);
@@ -21,7 +22,7 @@ inline vec3d force2_Van(vec3d dest, vec3d source, double sigma, double epsilon) 
 }
 
 inline vec3d force_Van(std::shared_ptr<std::vector<vec3d>> atoms, size_t desId, double sigma, double epsilon) {
-	vec3d force = { 0, 0, 0 };
+	vec3d force = { 0.0, 0.0, 0.0 };
 	for (size_t ii = 0; ii < atoms->size(); ++ii)
 		if (ii != desId)
 			force = force + force2_Van(atoms->operator[](desId), atoms->operator[](ii), sigma, epsilon);
@@ -34,7 +35,7 @@ inline double potential_Van(std::shared_ptr<std::vector<vec3d>> atoms, double si
 		for (int ii = 0; ii < jj; ++ii) {
 			auto r = atoms->operator[](ii) - atoms->operator[](jj);
 			auto r_mod = r.abs();
-			autp sigma_div_r = sigma / r_mod;
+			auto sigma_div_r = sigma / r_mod;
 			auto sdr_6 = pow(sigma_div_r, 6);
 			auto sdr_12 = sdr_6 * sdr_6;
 			potential += sdr_12 - sdr_6;
